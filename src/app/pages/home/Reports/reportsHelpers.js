421d21a07e63176ef37af53369698a25e5a372f7
Sergio Mendoza Matos <sergio_mdza@outlook.com>
2021-03-14 14:47:49 -0600

    Implementation of the TableComponent2 to all modules (#31)
    General changes:
    - A "mini refactor" was applied to all modules, deleting useless code and useless components. 
    - All `tableComponent`s from all modules were changed to the newest version: `tableComponent2`
    - The Report's General table was changed to a newer version implementing the same functioning and logic as the one on `tableComponent2` .
    
    Changes made on TableComponent2:
    - Columns `width` issue was fixed
    - Table `Height` issue was fixed
    - Added a new prop to hide all `searchBy` inputs
    - Added a new feature to hide `searchBy` inputs individually adding a new prop to the 'header' object.

--- src/app/pages/home/Reports/reportsHelpers.js
+++ src/app/pages/home/Reports/reportsHelpers.js
@@ -14,7 +14,7 @@ const _generalFields = {
   locations: ['name', 'level'],
   categories: ['name', 'depreciation'],
   references: ['name', 'brand', 'model', 'price'],
-  assets: ['name', 'brand', 'model', 'category', 'status', 'serial', 'responsible', 'notes', 'quantity', 'purchase_date', 'purchase_price', 'price', 'total_price','EPC', 'location', 'creator', 'creation_date', 'labeling_user', 'labeling_date'],
+  assets: ['name', 'brand', 'model', 'category', 'status', 'serial', 'responsible', 'notes', 'quantity', 'purchase_date', 'purchase_price', 'price', 'total_price', 'EPC', 'location', 'creator', 'creation_date', 'labeling_user', 'labeling_date'],
   depreciation: []
 };
 
@@ -22,12 +22,12 @@ export const formatCollection = (collectionName, completeFields) => {
   let customFieldNames = {};
   const rowToObjects = completeFields.map(row => {
     // Extract General Fields
-    const filteredGeneralFields =  extractGeneralField(collectionName, row);
+    const filteredGeneralFields = extractGeneralField(collectionName, row);
     // Extract Custom Fields with formatting
     let filteredCustomFields = {};
     const { customFieldsTab } = row;
     Object.values(customFieldsTab).forEach(tab => {
-      const allCustomFields = [ ...tab.left, ...tab.right ];
+      const allCustomFields = [...tab.left, ...tab.right];
       allCustomFields.map(field => {
         filteredCustomFields = { ...filteredCustomFields, ...extractCustomField(field) };
       });
@@ -39,6 +39,29 @@ export const formatCollection = (collectionName, completeFields) => {
   const normalizedRows = normalizeRows(rowToObjects, customFieldNames);
   // Convert rows to table presentation (Array for headers and every data row in an array)
   return convertRowsToDataTable(normalizedRows);
+};
+
+export const formatData = (collectionName, completeFields) => {
+  let customFieldNames = {};
+  const rowToObjects = completeFields.map(row => {
+    // Extract General Fields
+    const filteredGeneralFields = extractGeneralField(collectionName, row);
+    // Extract Custom Fields with formatting
+    let filteredCustomFields = {};
+    const { customFieldsTab } = row;
+    Object.values(customFieldsTab).forEach(tab => {
+      const allCustomFields = [...tab.left, ...tab.right];
+      allCustomFields.map(field => {
+        filteredCustomFields = { ...filteredCustomFields, ...extractCustomField(field) };
+      });
+    });
+    customFieldNames = { ...customFieldNames, ...filteredCustomFields };
+    return { ...filteredGeneralFields, ...filteredCustomFields };
+  });
+  // Make all rows homogenous filling missing custom fields
+  const normalizedRows = normalizeRows(rowToObjects, customFieldNames);
+  // Convert rows to table presentation (Array for headers and every data row in an array)
+  return convertRowsToDataTableObjects(normalizedRows);
 };
 
 const extractGeneralField = (collectionName, row) => {
@@ -53,20 +76,20 @@ const extractCustomField = field => {
   const { content, values } = field;
   if (isEmpty(values)) {
     return { [content]: '' };
-  } 
+  }
   const { fieldName, initialValue, options, selectedItem } = values;
   if (_types['simpleType'].includes(content)) {
     return { [fieldName]: initialValue || '' };
-  } else if(_types['dropType'].includes(content)) {
+  } else if (_types['dropType'].includes(content)) {
     return { [fieldName]: options[selectedItem] || '' };
-  } else if(_types['radioType'].includes(content)) {
+  } else if (_types['radioType'].includes(content)) {
     if (!selectedItem) return { [fieldName]: '' };
     const selected = selectedItem.slice(-1);
     return { [fieldName]: options[Number(selected) - 1] || '' };
-  } else if(_types['checkType'].includes(content)) {
-    const res  = options.reduce((acu, cur, ix) => values[`check${ix}`] ? acu += `${cur}|` : acu, '');
+  } else if (_types['checkType'].includes(content)) {
+    const res = options.reduce((acu, cur, ix) => values[`check${ix}`] ? acu += `${cur}|` : acu, '');
     return { [fieldName]: res.length ? res.slice(0, -1) : '' };
-  } else if(_types['fileType'].includes(content)) {
+  } else if (_types['fileType'].includes(content)) {
     return { [fieldName]: initialValue ? `path>${initialValue}` : '' };
   }
 };
@@ -74,15 +97,24 @@ const extractCustomField = field => {
 const normalizeRows = (rows, allCustomFields) => {
   return rows.map(row => {
     const missingCustomFields = difference(Object.keys(allCustomFields), Object.keys(row))
-    let missingCustomFieldsFormatted =  {};
-    missingCustomFields.forEach(field => missingCustomFieldsFormatted = { ...missingCustomFieldsFormatted, [field]: '' } );
+    let missingCustomFieldsFormatted = {};
+    missingCustomFields.forEach(field => missingCustomFieldsFormatted = { ...missingCustomFieldsFormatted, [field]: '' });
     return { ...row, ...missingCustomFieldsFormatted };
   });
 };
 
 const convertRowsToDataTable = rows => {
-  if (!rows || !Array.isArray(rows) || !rows.length)  return { header: [], tableRows: [] };
+  if (!rows || !Array.isArray(rows) || !rows.length) return { header: [], tableRows: [] };
   const header = Object.keys(rows[0]);
   const tableRows = rows.map(row => header.map(head => row[head]));
   return { header, tableRows }
+};
+
+const convertRowsToDataTableObjects = rows => {
+  if (!rows || !Array.isArray(rows) || !rows.length) return { header: [], tableRows: [] };
+  const header = Object.keys(rows[0]);
+  // eslint-disable-next-line no-labels
+  const headerObject = header.map((e) => ({ id: e, label: e, }));
+  const tableRows = rows.map(row => header.map(head => row[head]));
+  return { header, tableRows, headerObject, rows }
 };
